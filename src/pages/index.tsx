b53c3d7236023a81499b110a82f574dedc081197
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2023-11-02 22:53:53 +0900

    hello world

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "../components/layout";
-import gsap from "gsap";
+
 
 export default function Home() {
-
-  const initAnimation = (): void => {
-
-    const gatsbyColor = "#633194";
-    const tailwindColor = "#38BDF8";
-
-    gsap.fromTo(".background", {
-      background: gatsbyColor,
-    }, {
-      background: tailwindColor,
-      yoyo: true,
-      repeat: -1,
-      repeatDelay: 2
-    });
-
-    gsap.fromTo(".text", {
-      color: tailwindColor,
-    }, {
-      color: gatsbyColor,
-      yoyo: true,
-      repeat: -1,
-      repeatDelay: 2
-    });
-
-    console.log("init")
-  };
-  useEffect(() => {
-    initAnimation();
-  }, []);
-
-
   return (
     <>
       <Layout>
